acb1b25bccfc455402da3146726ff94b42d6d782
Velid Duranovic <velid.duranovic@infinitymesh.com>
2019-10-30 16:17:13 +0100

    Added fixed navigation header on window scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,11 +7,34 @@ hamburgerMenuOpenIcon.addEventListener("click", function(e){
         hamburgerMenuCloseIcon.style.display = "block";
         hamburgerMenuOpenIcon.style.display = "none";
     
-})
+});
 hamburgerMenuCloseIcon.addEventListener("click", function(e){
     var menu = document.getElementsByClassName("hamburger-menu")[0];
         menu.style.display = 'none';
         hamburgerMenuCloseIcon.style.display = "none";
         hamburgerMenuOpenIcon.style.display = "block";
 
-})
+});
+
+
+// Get the header
+var header = document.getElementById("header-head");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+
+window.addEventListener("scroll", function () {
+    myFunction();
+}, false);
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+    header.getElementsByClassName("header-logo")[0].children[0].setAttribute("src", "/themes/images/infinity-mesh-logo.png");
+  } else {
+    header.classList.remove("sticky");
+    header.getElementsByClassName("header-logo")[0].children[0].setAttribute("src", "/themes/images/header-logo.png");
+  }
+} 
