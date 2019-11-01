360b36936df385062ea89cfdcad03dd499b715cc
Velid Duranovic <velid.duranovic@infinitymesh.com>
2019-11-01 14:46:11 +0100

    Modified slick - from horizontal to vertical, and improved responsive behaviour

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,18 @@
 var hamburgerMenuOpenIcon = document.getElementsByClassName("hamburger-menu-icon-open")[0];
 var hamburgerMenuCloseIcon = document.getElementsByClassName("hamburger-menu-icon-close")[0];
-hamburgerMenuOpenIcon.addEventListener("click", function(e){
-    var menu = document.getElementsByClassName("hamburger-menu")[0];
-    
-        menu.style.display = 'block';
-        hamburgerMenuCloseIcon.style.display = "block";
-        hamburgerMenuOpenIcon.style.display = "none";
-    
+hamburgerMenuOpenIcon.addEventListener("click", function (e) {
+  var menu = document.getElementsByClassName("hamburger-menu")[0];
+
+  menu.style.display = 'block';
+  hamburgerMenuCloseIcon.style.display = "block";
+  hamburgerMenuOpenIcon.style.display = "none";
+
 });
-hamburgerMenuCloseIcon.addEventListener("click", function(e){
-    var menu = document.getElementsByClassName("hamburger-menu")[0];
-        menu.style.display = 'none';
-        hamburgerMenuCloseIcon.style.display = "none";
-        hamburgerMenuOpenIcon.style.display = "block";
+hamburgerMenuCloseIcon.addEventListener("click", function (e) {
+  var menu = document.getElementsByClassName("hamburger-menu")[0];
+  menu.style.display = 'none';
+  hamburgerMenuCloseIcon.style.display = "none";
+  hamburgerMenuOpenIcon.style.display = "block";
 
 });
 
@@ -25,7 +25,7 @@ var sticky = header.offsetTop;
 
 
 window.addEventListener("scroll", function () {
-    myFunction();
+  myFunction();
 }, false);
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
@@ -37,15 +37,30 @@ function myFunction() {
     header.classList.remove("sticky");
     header.getElementsByClassName("header-logo")[0].children[0].setAttribute("src", "/themes/images/header-logo.png");
   }
-} 
+}
 
 $('.slider').slick({
-  dots: true,
   infinite: true,
-  slidesToShow: 1,
-  vertical: false,
-  speed: 1000,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  speed: 2000,
+  dots: false,
   arrows: true,
-  autoplay: true
+  vertical: true,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  prevArrow: $('.slick-quotes-arrow-up'),
+  nextArrow: $('.slick-quotes-arrow-down'),
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        vertical: false,
+        dots: true
+      }
+    }
+  ]
 });
 
